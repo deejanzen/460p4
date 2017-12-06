fa15f41525c98e0c6d59db671a35174c7169b0e1
yuanma <yuanma@email.arizona.edu>
2017-12-06 14:42:07 -0700

    fixed the redirects

--- webapps/ROOT/WEB-INF/classes/dbController/DBController.java
+++ webapps/ROOT/WEB-INF/classes/dbController/DBController.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.*;
-import java.time.LocalDate;
+//import java.time.LocalDate;
 
 public class DBController {
 	final String userName ="tangmac";
